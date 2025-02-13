616c6e0beaac38c0625859413d39c452d5266816
Mr Hogback <mhudha9@gmail.com>
2025-02-13 13:48:29 +0700

    13/02/2025

--- public/data/dummyData.ts
+++ public/data/dummyData.ts
@@ -111,4 +111,84 @@ export const blogArticleDummy: BlogArticleProps[] = [
     articleQuote: { figure: "ligma", quote: "ballls" },
     outBoundLink: { title: "Medium", link: "https://medium.com/dialogika" },
   },
+  {
+    id: "mengapa-pitching-sangat-penting-dalam-dunia-bisnis-ini-dia-alasannya",
+    title: "Mengapa Pitching Sangat Penting dalam Dunia Bisnis? Ini Dia Alasannya!",
+    thumbnail: "https://i.pinimg.com/736x/0d/bd/66/0dbd66bf02b96a91e5f242eed49235bd.jpg",
+    metaData: "Pelajari cara pitching karna sangat penting bila ingin terjun kedalam dunia bisnis",
+    keywords: "Pelajari cara pitching karna sangat penting bila ingin terjun kedalam dunia bisnis",
+    cta: "pitching",
+    cardsDescription: "Kenali gaya dan cara untuk pitching investor-investormu bangh.",
+    content: "lorem ipsum, hellow world !",
+    images: [
+      {
+        name: "gambar-1",
+        src: "https://images.unsplash.com/photo-1515603403036-f3d35f75ca52?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      {
+        name: "gambar-1",
+        src: "https://i.pinimg.com/736x/0d/bd/66/0dbd66bf02b96a91e5f242eed49235bd.jpg",
+      },
+    ],
+    authors: [
+      {
+        authorName: "Jessica Caesya Agustin",
+        quotes: "hello world",
+        imgPath: "https://www.dialogika.co/blog/asset/img/intern/jessica-caesya-agustin.webp",
+      },
+      {
+        authorName: "Yoga Pangestu",
+        quotes:
+          "Jangan takut akan perubahan. Kita mungkin kehilangan sesuatu yang baik, namun kita akan peroleh sesuatu yang lebih baik lagi",
+        imgPath: "https://www.dialogika.co/blog/asset/img/intern/yoga-pangestu.jpg",
+      },
+    ],
+    writerNote:
+      "Artikel ini menjelaskan pentingnya komunikasi yang baik di tempat kerja untuk meningkatkan produktivitas dan mengurangi kesalahan. Keterampilan mendengarkan dengan baik, menyampaikan pesan dengan jelas, dan membangun budaya komunikasi yang terbuka sangat penting untuk menciptakan lingkungan kerja yang lancar",
+    publishedAt: new Date().toISOString(),
+    keyTakeaway: ["key Takeaway 1", "key takeaway 2"],
+    tags: ["tags 1", "tags 2"],
+    articleQuote: { figure: "ligma", quote: "ballls" },
+    outBoundLink: { title: "Medium", link: "https://medium.com/dialogika" },
+  },
+  {
+    id: "mengapa-pitching-sangat-penting-dalam-dunia-bisnis-ini-dia-alasannya",
+    title: "Mengapa Pitching Sangat Penting dalam Dunia Bisnis? Ini Dia Alasannya!",
+    thumbnail: "https://i.pinimg.com/736x/0d/bd/66/0dbd66bf02b96a91e5f242eed49235bd.jpg",
+    metaData: "Pelajari cara pitching karna sangat penting bila ingin terjun kedalam dunia bisnis",
+    keywords: "Pelajari cara pitching karna sangat penting bila ingin terjun kedalam dunia bisnis",
+    cta: "pitching",
+    cardsDescription: "Kenali gaya dan cara untuk pitching investor-investormu bangh.",
+    content: "lorem ipsum, hellow world !",
+    images: [
+      {
+        name: "gambar-1",
+        src: "https://images.unsplash.com/photo-1515603403036-f3d35f75ca52?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      {
+        name: "gambar-1",
+        src: "https://i.pinimg.com/736x/0d/bd/66/0dbd66bf02b96a91e5f242eed49235bd.jpg",
+      },
+    ],
+    authors: [
+      {
+        authorName: "Jessica Caesya Agustin",
+        quotes: "hello world",
+        imgPath: "https://www.dialogika.co/blog/asset/img/intern/jessica-caesya-agustin.webp",
+      },
+      {
+        authorName: "Yoga Pangestu",
+        quotes:
+          "Jangan takut akan perubahan. Kita mungkin kehilangan sesuatu yang baik, namun kita akan peroleh sesuatu yang lebih baik lagi",
+        imgPath: "https://www.dialogika.co/blog/asset/img/intern/yoga-pangestu.jpg",
+      },
+    ],
+    writerNote:
+      "Artikel ini menjelaskan pentingnya komunikasi yang baik di tempat kerja untuk meningkatkan produktivitas dan mengurangi kesalahan. Keterampilan mendengarkan dengan baik, menyampaikan pesan dengan jelas, dan membangun budaya komunikasi yang terbuka sangat penting untuk menciptakan lingkungan kerja yang lancar",
+    publishedAt: new Date().toISOString(),
+    keyTakeaway: ["key Takeaway 1", "key takeaway 2"],
+    tags: ["tags 1", "tags 2"],
+    articleQuote: { figure: "ligma", quote: "ballls" },
+    outBoundLink: { title: "Medium", link: "https://medium.com/dialogika" },
+  },
 ];
