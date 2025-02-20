593005c951314640e8a1a897d139c6e232ebe224
Yoga Pangestu <yogapangestu16@gmail.com>
2025-02-20 18:12:17 +0700

    add some intern images

--- components/article/editor/FormArticle.tsx
+++ components/article/editor/FormArticle.tsx
@@ -113,7 +113,7 @@ const FormArticle: React.FC<FormArticleProps> = ({ authors }) => {
     try {
       event.preventDefault();
       console.log("Submitting Article ...");
-      const res = await fetch("https://blog-ulby1hc3c-yoga723s-projects.vercel.app/blog/api/admin/article/", {
+      const res = await fetch("https://blog-yoga723s-projects.vercel.app/blog/api/admin/article/", {
         method: "POST",
         headers: {
           "Content-type": "application/json",
