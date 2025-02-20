5eeb2a6a30e3e2dfc177943d72820aafdc89847b
Yoga Pangestu <yogapangestu16@gmail.com>
2025-02-20 13:15:23 +0700

    20-02-2025

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 
 export const basePath = "/blog";
 
+const isStaticExport = process.env.NEXT_EXPORT === "true";
+
 const nextConfig: NextConfig = {
   trailingSlash: true,
-  // output: "export",
+  ...(isStaticExport ? { output: "export" } : {}),
 
   // Sesuaikan basePath dengan nama dari repository di github. Jika reponya about, maka "/about"
   basePath: "/blog",
