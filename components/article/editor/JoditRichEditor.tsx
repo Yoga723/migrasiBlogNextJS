14024f2d2a8c8275f9165c38ed6c9bc384059e56
Yoga Pangestu <yogapangestu16@gmail.com>
2025-02-22 22:26:33 +0700

    fix jodit editor

--- components/article/editor/JoditRichEditor.tsx
+++ components/article/editor/JoditRichEditor.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useMemo, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import dynamic from "next/dynamic";
 import "../../../app/styles/jodit-button-generator.css";
 // Import RichEditor secara dinamis (hanya di-render di client)
@@ -8,13 +8,16 @@ const JoditPro = dynamic(() => import("jodit-pro-react"), {
   loading: () => <p>Loading Editor...</p>,
 });
 const JoditRichEditor = () => {
-  const getLocalContent = localStorage.getItem("joditEditorContent");
   const editor = useRef<any>(null);
   const [editorValue, setEditorValue] = useState(
-    getLocalContent
-      ? getLocalContent
-      : `<p><span style="color: rgb(153, 153, 153);">ganti dengan keyword </span>- Mulai mengetik, ganti dengan isi artikel</p>`
+    `<p><span style="color: rgb(153, 153, 153);">ganti dengan keyword </span>- Mulai mengetik, ganti dengan isi artikel</p>`
   ); // Isi dari text/Jodit editor disimpan disini
+  useEffect(() => {
+    const savedContent = localStorage.getItem("joditEditorContent");
+    if (savedContent) {
+      setEditorValue(savedContent);
+    }
+  }, []);
   // gunakan joditConfig untuk setting jorditEditor. Baca dokumentasi : https://xdsoft.net/jodit/pro/docs/getting-started/usage.md
   const joditConfig = useMemo(
     () => ({
@@ -42,7 +45,7 @@ const JoditRichEditor = () => {
         tabIndex={1} // Manage focus
         onBlur={(newContent) => {
           setEditorValue(newContent);
-          if (getLocalContent) localStorage.setItem("joditEditorContent", newContent);
+          localStorage.setItem("joditEditorContent", newContent);
         }} // Update content onBlur
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         onChange={(newContent) => {}} // Nanti coba buat countdown 3 detik dan bila sudah 3 detik akan save ke draft di redux
